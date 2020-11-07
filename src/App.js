09b354d6d6360ae2f4a3e3be5a1d0d04313657d8
Pathum <pathummadushanka.weerathunga@student.kyh.se>
2020-11-07 21:16:12 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Tweet from './Tweet';
+let num = 0;
 
 function App(){
   return(
-    <div>
-      <h1>Hello React</h1>
+    <div className="app">
+      <Tweet name="Pathum" num="4"/>
+      <Tweet name="Senuli" num="40"/>
+      <Tweet name="Himansa" num="7"/>
+      <Tweet name="Senuli" num="72"/>
+      <Tweet name="Pathum" num="34"/>
     </div>
   );
 }
